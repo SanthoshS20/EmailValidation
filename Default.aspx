dd2dfae88c9faecac43d337da5ec655c7bc40cdb
Santhosh S <37366893+SanthoshS20@users.noreply.github.com>
2020-03-22 15:06:11 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <p><b>EMAIL VALIDATION WEB PAGE</b></p>
+    
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server" Height="35px" 
+            ontextchanged="TextBox1_TextChanged" Width="276px" BorderColor="#0033CC" 
+            CausesValidation="True" Font-Size="Medium"></asp:TextBox>
+    
+    <b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="Button1" runat="server" Height="35px" Text="Validate" 
+        Width="136px" BackColor="#3399FF" BorderColor="Blue" BorderStyle="Solid" 
+            Font-Bold="True" ForeColor="White" onclick="Button1_Click" />
+        </b>
+    
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string emailID;
+        int i,flag=0;
+        int emailNameCount = 0, atSymbolCount=0,nameCount=0;
+        emailID = TextBox1.Text;
+        int atFlag = 0, dotSymbol=0;
+        for (i = 0; i < emailID.Length; i++)
+        {
+            if (emailID[i] == '@')
+            {
+                atFlag = 1;
+                atSymbolCount += 1;
+                if (atSymbolCount > 1)
+                {
+                    flag = 1;
+                    break;
+                }
+                if (emailNameCount < 4)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            else if(emailID[i]=='.')
+            {
+                dotSymbol = 1;
+                if (nameCount < 2)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            else
+            {
+                if(atFlag==0)
+                    emailNameCount+=1;
+                else
+                    nameCount+=1;
+            }
+        }
+        if(dotSymbol==0 || atSymbolCount==0)
+            flag=1;
+
+        if (flag == 1)
+            Response.Write("<script>alert('Invalid Email ID')</script>");
+        else
+            Response.Write("<script>alert('Valid Email ID')</script>");
+    }
+}
